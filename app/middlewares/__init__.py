1d13c97a5ce591c324e4e0b30d76599d863480d9
Abstract-X <turinwhites@gmail.com>
2020-05-12 20:47:20 +1100

    Add cleaner

--- app/middlewares/__init__.py
+++ app/middlewares/__init__.py
@@ -1,9 +1,10 @@
 from aiogram import Bot, Dispatcher
 
 from .instances import InstancesMiddleware
+from app.services import Cleaner
 
 
-def setup(dispatcher: Dispatcher, bot: Bot):
+def setup(dispatcher: Dispatcher, bot: Bot, cleaner: Cleaner):
     """ Install middlewares. """
 
-    dispatcher.middleware.setup(InstancesMiddleware(bot, dispatcher))
+    dispatcher.middleware.setup(InstancesMiddleware(bot, dispatcher, cleaner))
--- app/middlewares/instances.py
+++ app/middlewares/instances.py
@@ -1,18 +1,22 @@
 from aiogram.dispatcher.middlewares import BaseMiddleware
 from aiogram import Bot, Dispatcher
 
+from app.services import Cleaner
+
 
 class InstancesMiddleware(BaseMiddleware):
     """ Middleware to add instances to handler signatures. """
 
-    def __init__(self, bot: Bot, dispatcher: Dispatcher):
+    def __init__(self, bot: Bot, dispatcher: Dispatcher, cleaner: Cleaner):
 
         super().__init__()
         self.bot = bot
         self.dispatcher = dispatcher
+        self.cleaner = cleaner
 
     async def on_process_message(self, _, data: dict):
 
         data["bot"] = self.bot
         data["dispatcher"] = self.dispatcher
+        data["cleaner"] = self.cleaner
         data["fsm_context"] = data["state"]
