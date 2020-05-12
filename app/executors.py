881b9c3e61eab4f4292bd8f5ee2a4aac3d7e42e6
Abstract-X <turinwhites@gmail.com>
2020-05-12 19:42:23 +1100

    Add execute_incorrect_chat, execute_chat_cleaning

--- app/executors.py
+++ app/executors.py
@@ -3,6 +3,7 @@ from aiogram.types import Message
 from aiogram.dispatcher import FSMContext
 
 from app import resources, FSMState
+from app.services import Cleaner
 
 
 async def execute_hello(update: Message, bot: Bot):
@@ -23,3 +24,40 @@ async def execute_request_chat(update: Message, bot: Bot, fsm_context: FSMContex
     )
 
     await fsm_context.set_state(FSMState.REQUEST_CHAT)
+
+
+async def execute_incorrect_chat(update: Message, bot: Bot):
+    """ Report that the chat data sent is incorrect. """
+
+    await bot.send_message(
+        chat_id=update.from_user.id,
+        text=resources.text.INCORRECT_CHAT_MESSAGE,
+        reply_to_message_id=update.message_id
+    )
+
+
+async def execute_chat_cleaning(update: Message, bot: Bot, cleaner: Cleaner):
+    """ Cleaning chat from all own messages. """
+
+    try:
+        chat_data = int(update.text)
+    except ValueError:
+        chat_data = update.text
+
+    await bot.send_message(
+        chat_id=update.from_user.id,
+        text=resources.text.CLEANING_STARTED_MESSAGE,
+        reply_to_message_id=update.message_id
+    )
+
+    report = await cleaner.clear_chat(chat_data)
+
+    await bot.send_message(
+        chat_id=update.from_user.id,
+        text=resources.text.CLEANING_FINISH_MESSAGE.format(
+            chat_name=report.entity_name,
+            chat_id=report.entity_id,
+            deleted_amount=report.amount_deleted_messages
+        ),
+        reply_to_message_id=update.message_id
+    )
