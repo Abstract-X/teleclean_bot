01e4469f99abbda73cda40e92021abaa2ea88449
Abstract-X <turinwhites@gmail.com>
2020-05-12 20:56:03 +1100

    Delete chat request again

--- app/handlers/request_chat_state.py
+++ app/handlers/request_chat_state.py
@@ -9,11 +9,7 @@ from app.services import Cleaner
 async def handle_chat_message(update: Message, bot: Bot, fsm_context: FSMContext, cleaner: Cleaner):
     """ Processing sent chat data. """
 
-    task_chat_cleaning = bot.loop.create_task(execute_chat_cleaning(update, bot, cleaner))
-    task_request_chat = bot.loop.create_task(execute_request_chat(update, bot, fsm_context))
-
-    await task_request_chat
-    await task_chat_cleaning
+    await execute_chat_cleaning(update, bot, cleaner)
 
 
 async def handle_incorrect_chat_message(update: Message, bot: Bot):
