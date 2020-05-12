1ac68f6fbc059d12eda6fe4b2f2f7d31f352f910
Abstract-X <turinwhites@gmail.com>
2020-05-12 20:16:23 +1100

    Add setup middlewares, registration handlers, cleaner runner

--- __main__.py
+++ __main__.py
@@ -4,25 +4,32 @@ from aiogram import Bot, Dispatcher, executor
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.types import ParseMode
 
-from app import config, middlewares
+from app import config, middlewares, handlers
+from app.services import Cleaner
 
 
 # instances
 loop = asyncio.get_event_loop()
 bot = Bot(token=config.BOT_TOKEN, parse_mode=ParseMode.HTML, loop=loop)
 dispatcher = Dispatcher(bot=bot, storage=MemoryStorage(), loop=loop)
+cleaner = Cleaner(config.TELEGRAM_APP_SESSION_NAME, config.TELEGRAM_APP_ID,
+                  config.TELEGRAM_APP_HASH, loop)
 
 
 async def on_startup(_):
     """ Executed before the bot starts. """
 
+    cleaner.connect()
+    user_id = await cleaner.load_user_id()
+
     middlewares.setup(dispatcher, bot)
+    handlers.register_all(dispatcher, user_id, cleaner)
 
 
 async def on_shutdown(_):
     """ Executed after the bot is turned off. """
 
-    ...
+    await cleaner.disconnect()
 
 
 # run
